950a8cd8a180d3ac0b104fd304a48b7794a1164f
Hanno Särg <wanaryytel@outlook.com>
2019-03-30 13:45:11 +0200

    Merge

--- wallet/wallet/urls.py
+++ wallet/wallet/urls.py
@@ -19,6 +19,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html')),
+    path('swedpay/', TemplateView.as_view(template_name='Swedbank.html')),
 
     path('admin/', admin.site.urls),
     path('', include('social_django.urls', namespace='social')),
